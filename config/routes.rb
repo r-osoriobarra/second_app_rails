65dffe5157beddbd24b939562b57f3600c7156a2
rodrigo <rob.developer@protonmail.com>
2021-07-16 17:38:51 -0400

    add: index view and test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do  
   get  "users/login"
   get  "pages/about_us"
+  get  "pages/test"
   root "pages#index"
 end
